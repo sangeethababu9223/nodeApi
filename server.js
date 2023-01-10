75c0542c1db2ee08d04c3eb05d6c3b73ebbd7fba
Nila <sangeetha9223@gmail.com>
2023-01-10 09:50:46 +0530

    basics of node req res

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const { headers, url, method } = req;
+  console.log(headers, url, method);
+  res.end();
 });
 const PORT = 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
