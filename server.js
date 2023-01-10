1dc7b58965f2926f0167b67bb03edf90c5f1d9e9
Nila <sangeetha9223@gmail.com>
2023-01-10 11:20:33 +0530

    Basic node API

--- server.js
+++ server.js
@@ -5,16 +5,40 @@ const todos = [
   { id: 3, text: "do three" },
 ];
 const server = http.createServer((req, res) => {
-  res.statusCode = 404;
-  res.setHeader("Content-Type", "application/json");
-  res.setHeader("X-Powered-By", "Node.js");
-  res.end(
-    JSON.stringify({
-      success: false,
-      error: "Not found",
-      data: null,
+  const { method, url } = req;
+  let body = [];
+  let status = 404;
+  const response = {
+    success: false,
+    data: null,
+  };
+  req
+    .on("data", (chunk) => {
+      body.push(chunk);
     })
-  );
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      if (method === "GET" && url === "/todos") {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === "POST" && url === "/todos") {
+        const { id, text } = JSON.parse(body);
+        if (!id || !text) {
+          status = 404;
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+      res.writeHead(status, {
+        "Content-Type": "application/json",
+        "X-Powered-By": "Node.js",
+      });
+      res.end(JSON.stringify(response));
+    });
 });
 const PORT = 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
