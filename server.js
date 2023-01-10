d449a94f08989bea5a8ee667dbeb2b38d1321eb0
Nila <sangeetha9223@gmail.com>
2023-01-10 10:13:28 +0530

    JSON data

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const http = require("http");
+const todos = [
+  { id: 1, text: "do one" },
+  { id: 2, text: "do two" },
+  { id: 3, text: "do three" },
+];
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Content-Type", "application/json");
   res.setHeader("X-Powered-By", "Node.js");
-  res.write("<h1>Hello</h1>");
-  res.write("<h2>Hello oi</h2>");
-  res.end();
+  res.end(
+    JSON.stringify({
+      success: true,
+      data: todos,
+    })
+  );
 });
 const PORT = 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
