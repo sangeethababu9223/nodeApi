524da9e6d0d60e7c88f16ae3310f61f13783a544
Nila <sangeetha9223@gmail.com>
2023-01-13 12:06:55 +0530

    Express routes, with html content

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const dotenv = require("dotenv");
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello from Express</h1>");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
